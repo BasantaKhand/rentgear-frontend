1a4a1185ef33edc7820cff5f15c58910c265144b
Basanta Khand <basantakhand55@gmail.com>
2026-06-01 01:54:00 +0000

    Add my bookings placeholder page

---
+++ src/pages/MyBookings.jsx
@@ -0,0 +1,12 @@
+function MyBookings() {
+  return (
+    <div className="main-content">
+      <div className="placeholder-page">
+        <h1>My Bookings</h1>
+        <p>Coming soon</p>
+      </div>
+    </div>
+  );
+}
+
+export default MyBookings;
