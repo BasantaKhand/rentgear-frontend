3fe9948a764dc6939561fd04e28c92aebeb1c6b0
Basanta Khand <basantakhand55@gmail.com>
2026-06-01 02:03:00 +0000

    Add profile placeholder page

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,12 @@
+function Profile() {
+  return (
+    <div className="main-content">
+      <div className="placeholder-page">
+        <h1>My Profile</h1>
+        <p>Coming soon</p>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
