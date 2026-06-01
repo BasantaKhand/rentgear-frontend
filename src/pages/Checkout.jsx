e1a23a67ad7bdd226abfa6a96ffc0995bc6f04bf
Basanta Khand <basantakhand55@gmail.com>
2026-06-01 01:51:00 +0000

    Add checkout placeholder page

---
+++ src/pages/Checkout.jsx
@@ -0,0 +1,12 @@
+function Checkout() {
+  return (
+    <div className="main-content">
+      <div className="placeholder-page">
+        <h1>Checkout</h1>
+        <p>Coming soon</p>
+      </div>
+    </div>
+  );
+}
+
+export default Checkout;
