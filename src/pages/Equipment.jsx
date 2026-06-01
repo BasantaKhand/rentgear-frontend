86fc28b40fe341fc28425ef6dc0834f0de9874d8
Basanta Khand <basantakhand55@gmail.com>
2026-06-01 01:42:00 +0000

    Add equipment listing placeholder page

---
+++ src/pages/Equipment.jsx
@@ -0,0 +1,12 @@
+function Equipment() {
+  return (
+    <div className="main-content">
+      <div className="placeholder-page">
+        <h1>Equipment</h1>
+        <p>Coming soon</p>
+      </div>
+    </div>
+  );
+}
+
+export default Equipment;
